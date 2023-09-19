6baf0913d1c478ab9342ae16c14c3a5491f32e67
Gregory Navasarkian (DevExpress) <gregoryn@devexpress.com>
2023-09-19 15:21:57 -0700

    hero/nav update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,47 +2,26 @@ import React from "react";
 import Typed from "react-typed";
 import { Link } from "react-router-dom";
 
+import house from "../assets/house.jpg"
+
 const Hero = () => {
   return (
-    <div className="bg-gradient-to-tl from-indigo-50 to-white">
-      <div className="max-w-[900px] mt-[-96] w-full min-h-screen mx-auto text-center flex flex-col justify-center">
-        <div className="flex justify-center items-center columns-2">
-          <div className="w-full">
-            <p className="text-indigo-900 font-bold md:text-2xl text-xl">
-              411 Starlight Crest
-            </p>
-            <h1 className="md:text-7xl sm:text-6xl text-indigo-900 text-4xl font-bold md:py-6 py-2">
-              Short description.
-            </h1>
-            <div className="flex justify-center items-center">
-              <p className="text-indigo-800 md:text-5xl sm:text-4xl text-xl font-bold">
-                Modern&nbsp;
-              </p>
-              <Typed
-                className="md:text-5xl sm:text-4xl text-xl font-bold text-indigo-900"
-                strings={[" design.", " electronics.", " styling."]}
-                typeSpeed={110}
-                backSpeed={130}
-                loop
-              />
-            </div>
-            <p className="md:text-2xl px-6 text-xl font-bold text-indigo-700 pt-6">
-              Long description.
-            </p>
-            <div className="p-8">
-              <Link to="/get-started">
-                <button className="bg-indigo-900 text-indigo-50 font-bold text-xl p-2 hover:text-indigo-200 transition-all duration-100 ease-in rounded-md">
-                  Gallery
-                </button>
-              </Link>
-            </div>
-          </div>
-          <div className="w-1/2">
-            image
-          </div>
+    <div class="bg-indigo-50 items-center mt-40 md:mt-36">
+      <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+          <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">481 Starlight Crest Dr</h1>
+          <p class="max-w-2xl mb-6 font-light text-indigo-800 lg:mb-8 md:text-lg lg:text-xl">Modern home with state of the art appliances and exquisite attention to detail.</p>
+          <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+            View Gallery
+            <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </a>
+          <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
+            Contact Us
+          </a>
         </div>
-
-
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <img src={house} alt="house" />
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,41 +13,42 @@ const Navbar = () => {
     { to: '/about', text: 'About' },
     { to: '/contact', text: 'Contact' },
   ];
-  
+
   return (
-    <div className="shadow-md w-full fixed top-0 left-0 z-10 bg-indigo-100">
-      <div className="md:flex items-center justify-between py-4 md:px-14 px-7">
-        <div className='flex items-center font-bold text-2xl cursor-pointer font-[Poppins] flex-shrink-0'>
-          <Link to='/'>Logo</Link>
-        </div>
-        <div
-          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
-          onClick={() => setOpen(!open)}
-        >
-          <img src={open ? close : menu} alt="menu" className="text-slate-950 " />
-        </div>
+    <header className='z-30'>
+      <div className="shadow-md w-full fixed top-0 left-0 bg-indigo-100">
+        <div className="md:flex items-center justify-between py-4 md:px-14 px-7">
+          <div className='flex items-center font-bold text-2xl cursor-pointer font-[Poppins] flex-shrink-0'>
+            <Link to='/'>Logo</Link>
+          </div>
+          <div
+            className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+            onClick={() => setOpen(!open)}
+          >
+            <img src={open ? close : menu} alt="menu" className="text-slate-950 " />
+          </div>
 
-        <ul className={`md:flex flex-shrink-0 md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 ${open
-          ? "top-20 opacity-100 transition-all duration-300 ease-in"
-          : "top-[-490px] transition-all duration-300 ease-in"
-          }`}>
-          {links.map((link, index) => (
-            <li key={index} className="md:py-0 py-4 md:px-0 px-4">
-              <Link
-                to={link.to}
-                className="text-indigo-950 font-[Poppins] font-bold text-xl p-2 hover:text-indigo-800 transition-all duration-100 ease-in"
-              >
-                {link.text}
-              </Link>
+          <ul className={`md:flex flex-shrink-0 md:items-center md:pb-0 pb-12 absolute md:static left-0 w-full md:w-auto md:pl-0 pl-9 ${open
+            ? "top-20 transition-all duration-300 ease-in"
+            : "top-[-490px] transition-all duration-300 ease-in"
+            }`}>
+            {links.map((link, index) => (
+              <li key={index} className="md:py-0 py-4 md:px-0 px-4">
+                <Link
+                  to={link.to}
+                  className="text-indigo-950 font-[Poppins] font-bold text-xl p-2 hover:text-indigo-800 transition-all duration-100 ease-in"
+                >
+                  {link.text}
+                </Link>
+              </li>
+            ))}
+            <li className='pl-5 md:pl-2'>
+              <button className="bg-indigo-900 text-indigo-50 font-[Poppins] font-bold text-xl p-2 hover:text-indigo-200 transition-all duration-100 ease-in rounded-md">Gallery</button>
             </li>
-          ))}
-          <li className='pl-5 md:pl-2'>
-            <button className="bg-indigo-900 text-indigo-50 font-[Poppins] font-bold text-xl p-2 hover:text-indigo-200 transition-all duration-100 ease-in rounded-md">Gallery</button>
-          </li>
-        </ul>
+          </ul>
 
-      </div>
-    </div>
+        </div>
+      </div></header>
   )
 }
 
