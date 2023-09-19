ce65db37e95417ab4652705cf63d4becd4a29849
Gregory Navasarkian (DevExpress) <gregoryn@devexpress.com>
2023-09-19 15:46:03 -0700

    hero/nav update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,55 +1,66 @@
-import React from 'react';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-scroll";
+import { FaTimes, FaBars } from "react-icons/fa";
 
-import menu from '../assets/menu.svg';
-import close from '../assets/close.svg';
 
 const Navbar = () => {
-  let [open, setOpen] = useState(false);
-
-  let links = [
-    { to: '/', text: 'Home' },
-    { to: '/about', text: 'About' },
-    { to: '/contact', text: 'Contact' },
-  ];
-
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  
+  const content = <>
+    <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-indigo-100 transition">
+      <ul className="text-center text-xl p-20">
+        <Link spy={true} smooth={true} to="Home">
+          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Home</li>
+        </Link >
+        <Link spy={true} smooth={true} to="Gallery">
+          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Gallery</li>
+        </Link>
+        <Link spy={true} smooth={true} to="About">
+          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">About</li>
+        </Link>
+        <Link spy={true} smooth={true} to="Contact">
+          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Contact</li>
+        </Link>
+      </ul>
+    </div>
+  </>
+  
   return (
-    <header className='z-30'>
-      <div className="shadow-md w-full fixed top-0 left-0 bg-indigo-100">
-        <div className="md:flex items-center justify-between py-4 md:px-14 px-7">
-          <div className='flex items-center font-bold text-2xl cursor-pointer font-[Poppins] flex-shrink-0'>
-            <Link to='/'>Logo</Link>
-          </div>
-          <div
-            className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
-            onClick={() => setOpen(!open)}
-          >
-            <img src={open ? close : menu} alt="menu" className="text-slate-950 " />
-          </div>
-
-          <ul className={`md:flex flex-shrink-0 md:items-center md:pb-0 pb-12 absolute md:static left-0 w-full md:w-auto md:pl-0 pl-9 ${open
-            ? "top-20 transition-all duration-300 ease-in"
-            : "top-[-490px] transition-all duration-300 ease-in"
-            }`}>
-            {links.map((link, index) => (
-              <li key={index} className="md:py-0 py-4 md:px-0 px-4">
-                <Link
-                  to={link.to}
-                  className="text-indigo-950 font-[Poppins] font-bold text-xl p-2 hover:text-indigo-800 transition-all duration-100 ease-in"
-                >
-                  {link.text}
-                </Link>
-              </li>
-            ))}
-            <li className='pl-5 md:pl-2'>
-              <button className="bg-indigo-900 text-indigo-50 font-[Poppins] font-bold text-xl p-2 hover:text-indigo-200 transition-all duration-100 ease-in rounded-md">Gallery</button>
-            </li>
-          </ul>
-
+    <nav>
+      <div className="h-10vh flex justify-between z-50 text-indigo-900 lg:py-5 px-4 md:px-20 py-4 shadow-md">
+        <div className="flex items-center flex-1">
+          <span className="text-3xl font-bold">Logo</span>
         </div>
-      </div></header>
-  )
-}
+        <div className="lg:flex md:flex lg:flex-1 items-center justify-end font-normal hidden">
+          <div className="flex-10">
+            <ul className="flex gap-8 mr-16 text-[18px]">
+              <Link spy={true} smooth={true} to="Home">
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Home</li>
+              </Link>
+              <Link spy={true} smooth={true} to="Gallery">
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Gallery</li>
+              </Link>
+              <Link spy={true} smooth={true} to="About">
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">About</li>
+              </Link>
+              <Link spy={true} smooth={true} to="Contact">
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Contact</li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+        <div>
+          {click && content}
+        </div>
+        
+        <button className="block sm:hidden transition" onClick={handleClick}>
+          {click ? <FaTimes /> : <FaBars />}
+        </button>
+
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
