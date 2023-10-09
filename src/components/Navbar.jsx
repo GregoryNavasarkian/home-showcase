f55ee1879688f62f02fa1d2e6e004a6ca318915f
Gregory Navasarkian (DevExpress) <gregoryn@devexpress.com>
2023-10-09 11:10:06 -0700

    hero/nav update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,16 +11,16 @@ const Navbar = () => {
     <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-indigo-100 transition">
       <ul className="text-center text-xl p-20">
         <Link spy={true} smooth={true} to="Home">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Home</li>
+          <li className="my-4 py-4 border-b border-indigo-800 hover:bg-indigo-800 hover:rounded">Home</li>
         </Link >
         <Link spy={true} smooth={true} to="Gallery">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Gallery</li>
+          <li className="my-4 py-4 border-b border-indigo-800 hover:bg-indigo-800 hover:rounded">Gallery</li>
         </Link>
         <Link spy={true} smooth={true} to="About">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">About</li>
+          <li className="my-4 py-4 border-b border-indigo-800 hover:bg-indigo-800 hover:rounded">About</li>
         </Link>
         <Link spy={true} smooth={true} to="Contact">
-          <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Contact</li>
+          <li className="my-4 py-4 border-b border-indigo-800 hover:bg-indigo-800 hover:rounded">Contact</li>
         </Link>
       </ul>
     </div>
@@ -36,16 +36,16 @@ const Navbar = () => {
           <div className="flex-10">
             <ul className="flex gap-8 mr-16 text-[18px]">
               <Link spy={true} smooth={true} to="Home">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Home</li>
+                <li className="hover:text-indigo-600 transition border-b-2 border-slate-900 hover:border-indigo-600 cursor-pointer">Home</li>
               </Link>
               <Link spy={true} smooth={true} to="Gallery">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Gallery</li>
+                <li className="hover:text-indigo-600 transition border-b-2 border-slate-900 hover:border-indigo-600 cursor-pointer">Gallery</li>
               </Link>
               <Link spy={true} smooth={true} to="About">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">About</li>
+                <li className="hover:text-indigo-600 transition border-b-2 border-slate-900 hover:border-indigo-600 cursor-pointer">About</li>
               </Link>
               <Link spy={true} smooth={true} to="Contact">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Contact</li>
+                <li className="hover:text-indigo-600 transition border-b-2 border-slate-900 hover:border-indigo-600 cursor-pointer">Contact</li>
               </Link>
             </ul>
           </div>
