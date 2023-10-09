84206d67e993af3a0dcb72aacfdcca2e35961afb
Gregory Navasarkian (DevExpress) <gregoryn@devexpress.com>
2023-10-09 14:18:50 -0700

    update gallery

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Gallery from './components/Gallery';
 
+
 const App = () => {
   return (
     <div className='landscape:lg:mt-0 landscape:md:mt-20 landscape:sm:mt-0 bg-indigo-200'>
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
+import house from "../assets/house.jpg"
+
 const images = [
   {
     original: "https://picsum.photos/id/1018/1000/600/",
@@ -15,14 +17,18 @@ const images = [
     original: "https://picsum.photos/id/1019/1000/600/",
     thumbnail: "https://picsum.photos/id/1019/250/150/",
   },
+  {
+    original: `${house}`,
+    thumbnail: `${house}`,
+  },
 ];
 
 const Gallery = () => {
   return (
-    <div className='md:mt-20 mt-10 mb-48 items-center justify-center bg-indigo-200'>
-      <h2 className='text-center md:text-7xl text-4xl font-bold text-indigo-950 drop-shadow-lg'>Gallery</h2>
-      <div className='flex items-center justify-center m-10 md:px-20 bg-indigo-200'>
-        <span className='content-center'><ImageGallery items={images} /></span>
+    <div className='pt-10 items-center justify-center bg-indigo-950'>
+      <h2 className='text-center md:text-6xl text-4xl font-bold text-indigo-100 drop-shadow-lg'>Gallery</h2>
+      <div className='flex items-center justify-center md:px-20 bg-indigo-950'>
+        <span className='content-center mb-14 m-8 md:min-w-[77%] min-w-[85%]'><ImageGallery items={images} /></span>
       </div>
     </div>
   )
