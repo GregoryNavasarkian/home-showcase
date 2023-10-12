a76ce7178068cf7366c176c24fb97910d6cc680f
Gregory Navasarkian <gregory.navasarkian@gmail.com>
2023-10-11 17:48:13 -0700

    add html support to md

--- src/pages/Security.jsx
+++ src/pages/Security.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactMarkdown from 'react-markdown';
 import { useNavigate } from "react-router-dom";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import rehypeRaw from 'rehype-raw';
 
 import MARKDOWN from './security.md';
 
@@ -12,7 +13,7 @@ const Security = () => {
     <div className="min-h-[95vh] flex justify-center bg-indigo-950">
       <button className="absolute text-white mr-2" onClick={() => navigate(-1)}> <ArrowLeftIcon /> Go Back</button>
       <div className="lg:w-3/5 w-4/5 bg-indigo-50 text-indigo-950 rounded-md shadow-md mt-24 mb-16 p-4 justify-center">
-        <ReactMarkdown className='prose'>{MARKDOWN}</ReactMarkdown>
+        <ReactMarkdown rehypePlugins={[rehypeRaw]} className='prose'>{MARKDOWN}</ReactMarkdown>
       </div>
     </div>
   );
