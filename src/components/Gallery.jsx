1ec48c1746dcdc68fcba87ddaec29ecdf7fcee44
Gregory Navasarkian <gregory.navasarkian@gmail.com>
2023-10-14 17:41:40 -0700

    add interior images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -28,7 +28,41 @@ import DSC_2583 from "../assets/backyardDark/DSC_2583.jpg";
 import DSC_2588 from "../assets/backyardDark/DSC_2588.jpg";
 import DSC_2589 from "../assets/backyardDark/DSC_2589.jpg";
 
-const backyardLightImages = [
+// interior
+import DSC_2592 from "../assets/interior/DSC_2592.jpg";
+import DSC_2593 from "../assets/interior/DSC_2593.jpg";
+import DSC_2595 from "../assets/interior/DSC_2595.jpg";
+import DSC_2596 from "../assets/interior/DSC_2596.jpg";
+import DSC_2597 from "../assets/interior/DSC_2597.jpg";
+import DSC_2598 from "../assets/interior/DSC_2598.jpg";
+import DSC_2601 from "../assets/interior/DSC_2601.jpg";
+import DSC_2602 from "../assets/interior/DSC_2602.jpg";
+import DSC_2604 from "../assets/interior/DSC_2604.jpg";
+import DSC_2606 from "../assets/interior/DSC_2606.jpg";
+import DSC_2607 from "../assets/interior/DSC_2607.jpg";
+import DSC_2610 from "../assets/interior/DSC_2610.jpg";
+import DSC_2614 from "../assets/interior/DSC_2614.jpg";
+import DSC_2615 from "../assets/interior/DSC_2615.jpg";
+import DSC_2617 from "../assets/interior/DSC_2617.jpg";
+import DSC_2618 from "../assets/interior/DSC_2618.jpg";
+import DSC_2619 from "../assets/interior/DSC_2619.jpg";
+import DSC_2621 from "../assets/interior/DSC_2621.jpg";
+import DSC_2623 from "../assets/interior/DSC_2623.jpg";
+import DSC_2631 from "../assets/interior/DSC_2631.jpg";
+import DSC_2632 from "../assets/interior/DSC_2632.jpg";
+import DSC_2633 from "../assets/interior/DSC_2633.jpg";
+import DSC_2634 from "../assets/interior/DSC_2634.jpg";
+import DSC_2635 from "../assets/interior/DSC_2635.jpg";
+import DSC_2637 from "../assets/interior/DSC_2637.jpg";
+import DSC_2639 from "../assets/interior/DSC_2639.jpg";
+import DSC_2640 from "../assets/interior/DSC_2640.jpg";
+import DSC_2641 from "../assets/interior/DSC_2641.jpg";
+import DSC_2644 from "../assets/interior/DSC_2644.jpg";
+import DSC_2645 from "../assets/interior/DSC_2645.jpg";
+import DSC_2646 from "../assets/interior/DSC_2646.jpg";
+import DSC_2647 from "../assets/interior/DSC_2647.jpg";
+
+const backyardImages = [
   {
     original: `${DSC_2525}`,
     thumbnail: `${DSC_2525}`,
@@ -141,6 +175,174 @@ const backyardLightImages = [
   },
 ];
 
+const interiorImages = [
+  {
+    original: `${DSC_2634}`,
+    thumbnail: `${DSC_2634}`,
+    description: "Front Entry",
+  },
+  {
+    original: `${DSC_2635}`,
+    thumbnail: `${DSC_2635}`,
+    description: "Front Entry",
+  },
+  {
+    original: `${DSC_2632}`,
+    thumbnail: `${DSC_2632}`,
+    description: "Front Entry",
+  },
+  {
+    original: `${DSC_2633}`,
+    thumbnail: `${DSC_2633}`,
+    description: "Front Entry",
+  },
+  {
+    original: `${DSC_2644}`,
+    thumbnail: `${DSC_2644}`,
+    description: "Dining Room",
+  },
+  {
+    original: `${DSC_2601}`,
+    thumbnail: `${DSC_2601}`,
+    description: "Dining Room To Kitchen",
+  },
+  {
+    original: `${DSC_2592}`,
+    thumbnail: `${DSC_2592}`,
+    description: "Kitchen",
+  },
+  {
+    original: `${DSC_2593}`,
+    thumbnail: `${DSC_2593}`,
+    description: "Kitchen",
+  },
+  {
+    original: `${DSC_2595}`,
+    thumbnail: `${DSC_2595}`,
+    description: "Kitchen",
+  },
+  {
+    original: `${DSC_2596}`,
+    thumbnail: `${DSC_2596}`,
+    description: "Kitchen",
+  },
+  {
+    original: `${DSC_2597}`,
+    thumbnail: `${DSC_2597}`,
+    description: "Kitchen",
+  },
+  {
+    original: `${DSC_2598}`,
+    thumbnail: `${DSC_2598}`,
+    description: "Kitchen ",
+  },
+  {
+    original: `${DSC_2631}`,
+    thumbnail: `${DSC_2631}`,
+    description: "Living Room",
+  },
+  {
+    original: `${DSC_2602}`,
+    thumbnail: `${DSC_2602}`,
+    description: "Living Room",
+  },
+  {
+    original: `${DSC_2647}`,
+    thumbnail: `${DSC_2647}`,
+    description: "Living Room",
+  },
+  {
+    original: `${DSC_2646}`,
+    thumbnail: `${DSC_2646}`,
+    description: "Living Room",
+  },
+  {
+    original: `${DSC_2645}`,
+    thumbnail: `${DSC_2645}`,
+    description: "Living Room Bar",
+  },
+  {
+    original: `${DSC_2602}`,
+    thumbnail: `${DSC_2602}`,
+    description: "Bathroom 1",
+  },
+  {
+    original: `${DSC_2604}`,
+    thumbnail: `${DSC_2604}`,
+    description: "Bathroom 1",
+  },
+  {
+    original: `${DSC_2615}`,
+    thumbnail: `${DSC_2615}`,
+    description: "Master Bedroom",
+  },
+  {
+    original: `${DSC_2617}`,
+    thumbnail: `${DSC_2617}`,
+    description: "Master Bedroom",
+  },
+  {
+    original: `${DSC_2614}`,
+    thumbnail: `${DSC_2614}`,
+    description: "Master Closet",
+  },
+  {
+    original: `${DSC_2606}`,
+    thumbnail: `${DSC_2606}`,
+    description: "Master Bathroom",
+  },
+  {
+    original: `${DSC_2607}`,
+    thumbnail: `${DSC_2607}`,
+    description: "Master Bathroom",
+  },
+  {
+    original: `${DSC_2621}`,
+    thumbnail: `${DSC_2621}`,
+    description: "Master Bathroom",
+  },
+  {
+    original: `${DSC_2610}`,
+    thumbnail: `${DSC_2610}`,
+    description: "Master Bathroom",
+  },
+  {
+    original: `${DSC_2637}`,
+    thumbnail: `${DSC_2637}`,
+    description: "Bedroom 1",
+  },
+  {
+    original: `${DSC_2639}`,
+    thumbnail: `${DSC_2639}`,
+    description: "Bedroom 1",
+  },
+  {
+    original: `${DSC_2640}`,
+    thumbnail: `${DSC_2640}`,
+    description: "Bedroom 1",
+  },
+  {
+    original: `${DSC_2641}`,
+    thumbnail: `${DSC_2641}`,
+    description: "Bedroom 1",
+  },
+  {
+    original: `${DSC_2623}`,
+    thumbnail: `${DSC_2623}`,
+    description: "Security Panel",
+  },
+  {
+    original: `${DSC_2619}`,
+    thumbnail: `${DSC_2619}`,
+    description: "iPad Touch Panel",
+  },
+  {
+    original: `${DSC_2618}`,
+    thumbnail: `${DSC_2618}`,
+    description: "Thermostat",
+  }
+];
+
 const Gallery = () => {
   return (
     <div
@@ -152,7 +354,7 @@ const Gallery = () => {
       </h2>
       <div className="flex items-center justify-center md:px-20 bg-indigo-950">
         <span className="content-center mb-20 m-8 md:w-[70%] w-[85%]">
-          <ImageGallery items={backyardLightImages} />
+          <ImageGallery items={backyardImages} />
         </span>
       </div>
       <h2 className="text-center md:text-6xl text-4xl text-indigo-100 drop-shadow-lg">
@@ -160,7 +362,7 @@ const Gallery = () => {
       </h2>
       <div className="flex items-center justify-center md:px-20 bg-indigo-950">
         <span className="content-center mb-20 m-8 md:w-[70%] w-[85%]">
-          <ImageGallery items={backyardLightImages} />
+          <ImageGallery items={interiorImages} />
         </span>
       </div>
     </div>
